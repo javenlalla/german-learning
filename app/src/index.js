9e19d1226cfd60794c40d0fb52d21a8e6e045bc8
Javen Lalla <javen.lalla@gmail.com>
2022-06-04 15:18:12 +0200

    feat: implement basic analytics

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter,
   Routes,
   Route,
+  Switch,
+  useLocation,
+  withRouter,
 } from "react-router-dom";
+import { MatomoProvider, createInstance, useMatomo } from '@jonkoops/matomo-tracker-react'
 import App from './App';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
@@ -29,34 +33,85 @@ import Lesen from "./routes/Lesen";
 import Preposition from "./routes/Preposition";
 import Zu from "./routes/Zu";
 
+const instance = createInstance({
+  urlBase: "https://analytics.jlalla.com",
+  siteId: 1,
+  // linkTracking: false,
+  configurations: {
+    disableCookies: true,
+  }
+});
+
+function usePageViews() {
+  const { trackPageView, trackEvent, enableLinkTracking  } = useMatomo()
+  const location = useLocation()
+
+  // enableLinkTracking();
+
+  useEffect(() => {
+    // console.log("track page view", location.pathname);
+    // trackPageView({
+    //   documentTitle: 'Page title', // optional
+    //   href: "/briefschreiben",
+    // });
+
+    trackPageView();
+
+
+  }, [location]);
+
+  // useEffect(
+  //   () => {
+  //     ga.send(['pageview', location.pathname])
+  //   },
+  //   [location]
+  // )
+}
+
+const Main = () => {
+  usePageViews();
+
+  // const { enableLinkTracking } = useMatomo()
+  //
+  // enableLinkTracking()
+
+  return (
+      <Routes>
+        <Route path="/" element={<App/>}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="deklination" element={<Deklination/>}/>
+          <Route path="konjunktiv-ii" element={<KonjunktivII/>}/>
+          <Route path="planen" element={<Planen/>}/>
+          <Route path="konjunktionen" element={<Konjunktionen/>}/>
+          <Route path="wohin-wo-woher" element={<Wo/>}/>
+          <Route path="werden" element={<Werden/>}/>
+          <Route path="vokabular" element={<Vokabular/>}/>
+          <Route path="lassen" element={<Lassen/>}/>
+          <Route path="indirekt-fragen" element={<IndirektFragen/>}/>
+          <Route path="prufung-punkte" element={<TestPunkte/>}/>
+          <Route path="brief-schreiben" element={<BriefSchreiben/>}/>
+          <Route path="bildbeschreibung" element={<Bildbeschreibung/>}/>
+          <Route path="modalverben" element={<ModalVerben/>}/>
+          <Route path="als" element={<Als/>}/>
+          <Route path="passiv" element={<Passiv/>}/>
+          <Route path="lesen" element={<Lesen/>}/>
+          <Route path="prepositions" element={<Preposition/>}/>
+          <Route path="zu" element={<Zu/>}/>
+        </Route>
+      </Routes>
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <MatomoProvider value={instance}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App/>}>
-            <Route path="/" element={<Home/>}/>
-            <Route path="deklination" element={<Deklination/>}/>
-            <Route path="konjunktiv-ii" element={<KonjunktivII/>}/>
-            <Route path="planen" element={<Planen/>}/>
-            <Route path="konjunktionen" element={<Konjunktionen/>}/>
-            <Route path="wohin-wo-woher" element={<Wo/>}/>
-            <Route path="werden" element={<Werden/>}/>
-            <Route path="vokabular" element={<Vokabular/>}/>
-            <Route path="lassen" element={<Lassen/>}/>
-            <Route path="indirekt-fragen" element={<IndirektFragen/>}/>
-            <Route path="prufung-punkte" element={<TestPunkte/>}/>
-            <Route path="brief-schreiben" element={<BriefSchreiben/>}/>
-            <Route path="bildbeschreibung" element={<Bildbeschreibung/>}/>
-            <Route path="modalverben" element={<ModalVerben/>}/>
-            <Route path="als" element={<Als/>}/>
-            <Route path="passiv" element={<Passiv/>}/>
-            <Route path="lesen" element={<Lesen/>}/>
-            <Route path="prepositions" element={<Preposition/>}/>
-            <Route path="zu" element={<Zu/>}/>
-          </Route>
-        </Routes>
+
+      <Main/>
       </BrowserRouter>
+      </MatomoProvider>
+
     </Provider>
   </React.StrictMode>,
   document.getElementById('app')
